030e22b0e54b502380760358a46210d31aec4c59
Jos de Jong <wjosdejong@gmail.com>
2025-11-21 14:44:00 +0100

    fix: indentation and alignment of nested tables

--- tabularjson/stringify.py
+++ tabularjson/stringify.py
@@ -53,7 +53,7 @@ def stringify(data: Any, options: StringifyOptions | None = None) -> str:
     )
     trailing_commas = (options.get("trailingCommas") if options else False) or False
 
-    def stringify_value(value: Any, indent: str, indentation: str | None) -> str:
+    def stringify_value(value: Any, indent: str, do_indent: bool) -> str:
         # number
         if type(value) is int or type(value) is float:
             if isnan(value):
@@ -73,45 +73,50 @@ def stringify(data: Any, options: StringifyOptions | None = None) -> str:
 
         # table
         if is_tabular(value):
-            return stringify_table(value, indent, indentation)
+            return stringify_table(value, indent)
 
         # array
         if type(value) is list:
-            return stringify_array(value, indent, indentation)
+            return stringify_array(value, indent, do_indent)
 
         # object
         if type(value) is dict:
-            return stringify_object(value, indent, indentation)
+            return stringify_object(value, indent, do_indent)
 
         raise TypeError("Unknown type of data: " + str(type(value)))
 
-    def stringify_array(array: list[Any], indent: str, indentation: str | None):
+    def stringify_array(array: list[Any], indent: str, do_indent: bool):
         if len(array) == 0:
             return "[]"
 
-        child_indent = (indent + indentation) if indentation else indent
-        text = "[\n" if indentation else "["
+        child_indent = (indent + global_indentation) if do_indent else indent
+        text = "[\n" if do_indent else "["
 
         for index, item in enumerate(array):
-            if indentation:
+            if do_indent:
                 text += child_indent
 
             if type(item) is not Function:
-                text += stringify_value(item, child_indent, indentation)
+                text += stringify_value(item, child_indent, do_indent)
 
             if index < len(array) - 1:
-                text += ",\n" if indentation else ","
+                text += ",\n" if do_indent else ","
             elif trailing_commas:
                 text += ","
 
-        text += "\n" + indent + "]" if indentation else "]"
+        text += "\n" + indent + "]" if do_indent else "]"
 
         return text
 
-    def stringify_table(array: list[Any], indent: str, indentation: str | None):
+    def stringify_table(array: list[Any], indent: str):
         is_root = array == data
-        child_indent = (indent + indentation) if (indentation and indent) else indent
-        col_separator = ", " if indentation else ","
+        table_do_indent = global_indentation != ""
+        child_indent = (
+            (indent + global_indentation)
+            if (table_do_indent and not is_root)
+            else indent
+        )
+        col_separator = ", " if table_do_indent else ","
 
         fields = get_fields(array)
 
@@ -119,7 +124,10 @@ def stringify(data: Any, options: StringifyOptions | None = None) -> str:
 
         def stringify_cell(item: Any, field: TableFieldGetter):
             value, exists = field["get_value"](item)
-            return stringify_value(value, child_indent, None) if exists else ""
+
+            # We pass do_indent=False so nested objects/arrays are not formatted over multiple lines.
+            # Nested tables though are always indented (when global_indentation is set).
+            return stringify_value(value, child_indent, False) if exists else ""
 
         header = list(map(lambda field: field["name"], fields))
         rows = list(
@@ -134,7 +142,7 @@ def stringify(data: Any, options: StringifyOptions | None = None) -> str:
             )
         )
 
-        if indentation:
+        if table_do_indent:
             widths = calculate_column_widths(header, rows)
 
             text += child_indent + format_row(header, widths)
@@ -159,31 +167,31 @@ def stringify(data: Any, options: StringifyOptions | None = None) -> str:
 
         return "".join(cells)
 
-    def stringify_object(obj: Record, indent: str, indentation: str | None):
+    def stringify_object(obj: Record, indent: str, do_indent: bool):
         entries = obj.items()
 
         if len(entries) == 0:
             return "{}"
 
-        child_indent = indent + indentation if indentation else indent
-        text = "{\n" if indentation else "{"
+        child_indent = indent + global_indentation if do_indent else indent
+        text = "{\n" if do_indent else "{"
 
         for index, (key, value) in enumerate(entries):
             key_str = stringify_primitive_value(key)
 
-            text += child_indent + key_str + ": " if indentation else key_str + ":"
-            text += stringify_value(value, child_indent, indentation)
+            text += child_indent + key_str + ": " if do_indent else key_str + ":"
+            text += stringify_value(value, child_indent, do_indent)
 
             if index < len(entries) - 1:
-                text += ",\n" if indentation else ","
+                text += ",\n" if do_indent else ","
             elif trailing_commas:
                 text += ","
 
-        text += "\n" + indent + "}" if indentation else "}"
+        text += "\n" + indent + "}" if do_indent else "}"
 
         return text
 
-    return stringify_value(data, "", global_indentation)
+    return stringify_value(data, "", global_indentation != "")
 
 
 def get_fields(records: list[Any]) -> list[TableFieldGetter]:
@@ -216,14 +224,14 @@ def stringify_field(path: Path):
     return ".".join(map(lambda key: stringify_primitive_value(key), path))
 
 
-def resolve_indentation(indentation: int | str | None) -> str | None:
+def resolve_indentation(indentation: int | str | None) -> str:
     if type(indentation) is int:
         return " " * indentation
 
     if type(indentation) is str and indentation != "":
         return indentation
 
-    return None
+    return ""
 
 
 def calculate_column_widths(header: list[str], rows: list[list[str]]) -> list[int]:
